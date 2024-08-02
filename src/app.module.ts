d7304e574399f719dfbb38e462f93eca544d4e7c
has673 <hassansajjas>
2024-08-02 15:19:18 +0500

    category

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmployeesModule } from './employees/employees.module';
 import { DatabaseModule } from './database/database.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [EmployeesModule, DatabaseModule],
+  imports: [EmployeesModule, DatabaseModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/categories/categories.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import { DatabaseService } from 'src/database/database.service';
+import { Prisma } from '@prisma/client';
+
+
+@Injectable()
+export class CategoriesService {
+  constructor(private readonly datbaseService:DatabaseService){}
+
+  create(createCategoryDto: Prisma.CategoryCreateInput) {
+    return this.datbaseService.category.create({
+      data:createCategoryDto
+    });
+  }
+
+  findAll() {
+    return this.datbaseService.category.findMany();
+  }
+
+  findOne(id: number) {
+    return this.datbaseService.category.findFirst({
+      where:{
+        id,
+      }
+    });
+  }
+
+  update(id: number, updateCategoryDto: Prisma.CategoryUpdateInput) {
+    return this.datbaseService.category.update({
+      where:{
+        id,
+      },data:updateCategoryDto
+    });
+  }
+
+  remove(id: number) {
+    return this.datbaseService.category.delete({
+      where:{
+        id
+      }
+    });
+  }
+}
