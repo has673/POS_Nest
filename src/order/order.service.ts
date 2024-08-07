29b6137debf061df308e882b77a5dbbcaca16315
has673 <hassansajjas>
2024-08-07 10:54:37 +0500

    module

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,26 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
 export class OrderService {
+  constructor(private readonly databaseService:DatabaseService){}
   create(createOrderDto: CreateOrderDto) {
     return 'This action adds a new order';
   }
 
-  findAll() {
-    return `This action returns all order`;
+ async  findAll() {
+    return await this.databaseService.order.findMany() ;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} order`;
+ async  findOne(id: number) {
+    return await this.databaseService.order.findFirst({
+      where:{
+        id
+      }
+    });
   }
 
   update(id: number, updateOrderDto: UpdateOrderDto) {
     return `This action updates a #${id} order`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} order`;
+  async remove(id: number) {
+    return await this.databaseService.order.delete({
+      where:{
+        id
+      }
+    });
   }
 }
